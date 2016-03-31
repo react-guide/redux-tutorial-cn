f4e16063807960b1069d9aa9a66f1d2605a624be
Sinux <nsinux@gmail.com>
2016-03-31 18:17:58 +0800

    Revert "Finished the chapter 12"
    This reverts commit b78a681e7c926822492a3ae9fc5154d0b46a97dd.

--- 12_final-words.js
+++ 12_final-words.js
@@ -1,15 +1,15 @@
 // 章节 12 - final-words.js
 
-// 除了本教程，其实 Redux 和 react-redux 还有很多可以学习的。比如:
-// 考虑到 Redux 的安全性，你可能会对 bindActionCreators 感兴趣（创建一个已绑定到 dispatch 的 action creators
-// 散列值 -http://rackt.org/redux/docs/api/bindActionCreators.html).
+// There is actually more to Redux and react-redux than what we showed you with this tutorial. For example,
+// concerning Redux, you may be interested in bindActionCreators (to produce a hash of action creators
+// already bound to dispatch - http://rackt.org/redux/docs/api/bindActionCreators.html).
 
-// 希望我们已经给了你一些秘诀去更好的理解 Flux ，以及 Flux 不同实现之间的区别
-// --当然包括 Redux 是如何脱颖而出的 ;)。
+// We hope we've given you the keys to better understand Flux and to see more clearly
+// how Flux implementations differ from one another--especially how Redux stands out ;).
 
-// 接下去呢?
+// Where to go from here?
 
-// Redux 的官方文档真的非常出色和详尽，所以从现在开始不要再犹豫要不要看它了：
-// http://rackt.org/redux/index.html
+// The official Redux documentation is really awesome and exhaustive so you should not hesitate to
+// refer to it from now on: http://rackt.org/redux/index.html
 
-// 祝你和 React、Redux 玩的开心2333!
+// Have fun with React and Redux!
