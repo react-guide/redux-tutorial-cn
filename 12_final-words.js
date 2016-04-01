d275baf2025bbd282b92a7a029c9ef906f9be465
Sinux <nsinux@gmail.com>
2016-04-01 17:52:31 +0800

    Change raw hash to hash Object

--- 12_final-words.js
+++ 12_final-words.js
@@ -2,7 +2,7 @@
 
 // 除了本教程，其实 Redux 和 react-redux 还有很多可以学习的。比如:
 // 考虑到 Redux 的安全性，你可能会对 bindActionCreators 感兴趣（创建一个已绑定到 dispatch 的 action creators
-// 散列值 -http://rackt.org/redux/docs/api/bindActionCreators.html).
+// hash 对象 -http://rackt.org/redux/docs/api/bindActionCreators.html)。
 
 // 希望我们已经给了你一些秘诀去更好的理解 Flux ，以及 Flux 不同实现之间的区别
 // --当然包括 Redux 是如何脱颖而出的 ;)。
